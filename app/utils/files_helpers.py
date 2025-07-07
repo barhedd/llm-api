3eb4d88c9dfaed1772dc678c496e7c330a8ed874
Eddy Barahona <barahona.hernandez2712@gmail.com>
2025-07-07 02:05:06 -0600

    refactor: remove unusing methods

--- app/utils/files_helpers.py
+++ app/utils/files_helpers.py
@@ -1,11 +1,8 @@
 import os
-import csv
 import json
 from datetime import datetime
 from typing import List, Dict, Any
 from uuid import uuid4
-
-from app.core import config
 
 def read_news_by_dates(filepath: str, dates: List[str]) -> List[Dict[str, str]]:
     news = []
@@ -24,38 +21,6 @@ def read_news_by_dates(filepath: str, dates: List[str]) -> List[Dict[str, str]]:
             news.append(noticia)
 
     return news
-
-def read_news_from_json_by_date(date: str) -> List[Dict[str, str]]:
-    noticias_filtradas = []
-    if not os.path.exists(config.NEWS_JSON_FILE):
-        return noticias_filtradas
-
-    with open(config.NEWS_JSON_FILE, "r", encoding="utf-8") as f:
-        try:
-            all_news = json.load(f)
-        except json.JSONDecodeError:
-            print("❌ Error al cargar el JSON de noticias.")
-            return []
-
-    for noticia in all_news:
-        if noticia.get("fecha") == date:
-            noticias_filtradas.append(noticia)
-
-    return noticias_filtradas
-
-def save_results_in_csv(resultados: List[dict]):
-    nuevo_archivo = not os.path.exists(config.CSV_FILE)
-
-    with open(config.CSV_FILE, mode="a", newline="", encoding="utf-8") as file:
-        writer = csv.DictWriter(file, fieldnames=["fecha", "respuesta_cruda"])
-        if nuevo_archivo:
-            writer.writeheader()
-
-        for resultado in resultados:
-            writer.writerow({
-                "fecha": resultado["fecha"],
-                "respuesta_cruda": resultado["respuesta_cruda"]
-            })
 
 def save_news_in_json(json_output: List[Dict[str, Any]]) -> str:
     timestamp = datetime.now().strftime("%Y-%m-%d")
